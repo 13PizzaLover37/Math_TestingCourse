1fa6005259e165f4b8867990df421edeef767d7f
Валерий Кучерявый <kucheryavyi.valeriy@gmail.com>
2024-12-11 22:54:06 +0200

    Tests: grading calculator tests added

---
+++ CalculatorNUnitTest/GradingCalculatorNUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using Calculator;
+using NUnit.Framework;
+
+namespace Math
+{
+    [TestFixture]
+    public class GradingCalculatorNUnitTests
+    {
+        private GradingCalculator _gradingCalculator;
+
+        [SetUp]
+        public void Setup() => _gradingCalculator = new GradingCalculator();
+
+        [Test]
+        [TestCase(95, 90, ExpectedResult = "A")]
+        [TestCase(85, 90, ExpectedResult = "B")]
+        [TestCase(65, 90, ExpectedResult = "C")]
+        [TestCase(95, 65, ExpectedResult = "B")]
+        [TestCase(95, 55, ExpectedResult = "D")]
+        [TestCase(65, 55, ExpectedResult = "D")]
+        [TestCase(50, 90, ExpectedResult = "D")]
+        public string GetGrade_ReceiveCorrectAnswers(int score, int attendancePercentage)
+        {
+            return _gradingCalculator.GetGrade(score, attendancePercentage);
+        }
+    }
+}
