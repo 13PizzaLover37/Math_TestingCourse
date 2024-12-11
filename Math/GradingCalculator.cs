b7c331ce937a55ac23d33048a1fd461eb7bc40ba
Валерий Кучерявый <kucheryavyi.valeriy@gmail.com>
2024-12-11 22:53:39 +0200

    Add: grading calculator model added

---
+++ Math/GradingCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculator
+{
+    public class GradingCalculator
+    {
+        public string GetGrade(int score, int attendancePercentage)
+        {
+            if (score > 90 && attendancePercentage > 70) return "A";
+            else if (score > 80 && attendancePercentage > 60) return "B";
+            else if (score > 60 && attendancePercentage > 60) return "C";
+            else return "F";
+        }
+    }
+}
